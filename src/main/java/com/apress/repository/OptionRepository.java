86acb6c91d6975dda1d72fcaf9e31ac75421c577
Sherlock <user@Sherlock.local>
2020-06-03 10:59:10 +0100

    HSQLDB dependency added

--- src/main/java/com/apress/repository/OptionRepository.java
+++ src/main/java/com/apress/repository/OptionRepository.java
@@ -1,3 +1,7 @@
 package com.apress.repository;
-import org.springframework.data.repository.CrudRepository; import com.apress.domain.Option;
-public interface OptionRepository extends CrudRepository<Option, Long> { }
+import org.springframework.data.repository.CrudRepository; 
+import com.apress.domain.Option;
+
+public interface OptionRepository extends CrudRepository<Option, Long> { 
+    
+}
