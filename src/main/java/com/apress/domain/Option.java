cdb436c97bcf9225bd15c04789ce3f6630def0f8
Sherlock <user@Sherlock.local>
2020-06-03 10:24:49 +0100

    generate Poll class

--- src/main/java/com/apress/domain/Option.java
+++ src/main/java/com/apress/domain/Option.java
@@ -1,9 +1,12 @@
 package com.apress.domain;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue; import javax.persistence.Id;
+import javax.persistence.GeneratedValue; 
+import javax.persistence.Id;
+// To declare this class is a database
 @Entity
 public class Option {
+// to declare this is my primary key?
 @Id
 @GeneratedValue @Column(name="OPTION_ID") private Long id;
 @Column(name="OPTION_VALUE") private String value;
