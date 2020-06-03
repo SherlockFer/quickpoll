2f5fccd4d93d56ea96cf25675e55b8ec71b7da77
SherlockFer <fermin.aguilar.b@gmail.com>
2020-06-03 10:37:56 +0100

    Option, Poll and Vote interface created

---
+++ src/main/java/com/apress/repository/OptionRepository.java
@@ -0,0 +1,3 @@
+package com.apress.repository;
+import org.springframework.data.repository.CrudRepository; import com.apress.domain.Option;
+public interface OptionRepository extends CrudRepository<Option, Long> { }
