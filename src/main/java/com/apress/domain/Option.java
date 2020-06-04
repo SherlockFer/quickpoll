5c4fb03644b30549630c6ab0b5d525d5d08f03a2
SherlockFer <fermin.aguilar.b@gmail.com>
2020-06-04 13:50:01 +0100

    Adding getting and setters

--- src/main/java/com/apress/domain/Option.java
+++ src/main/java/com/apress/domain/Option.java
@@ -14,4 +14,21 @@ public class Option {
 
 	@Column(name = "OPTION_VALUE")
 	private String value;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 }
