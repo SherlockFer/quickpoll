ea5a2471e95dbfd25854dc21a84845ee611261f4
user <user@192.168.0.115>
2020-06-04 19:38:34 +0100

    VoteController -> GET All Votes Implementation

--- src/main/java/com/apress/repository/VoteRepository.java
+++ src/main/java/com/apress/repository/VoteRepository.java
@@ -1,5 +1,6 @@
 package com.apress.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +8,8 @@ import com.apress.domain.Vote;
 
 @Repository
 public interface VoteRepository extends CrudRepository<Vote, Long> {
+
+	@Query(value = "select v.* from Option o, Vote v where o.POLL_ID = ?1 and v.OPTION_ID = o.OPTION_ID", nativeQuery = true)
+	public Iterable<Vote> findByPoll(Long pollId);
 
 }
