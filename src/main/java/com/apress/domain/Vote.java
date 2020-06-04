b1ef5a0a823ac6fb5d7414da5e5279e9eba527ce
user <user@192.168.0.115>
2020-06-04 18:52:27 +0100

    Poll Controller -> methods GET, PUT and DELETE created

--- src/main/java/com/apress/domain/Vote.java
+++ src/main/java/com/apress/domain/Vote.java
@@ -17,4 +17,21 @@ public class Vote {
 	@ManyToOne
 	@JoinColumn(name = "OPTION_ID")
 	private Option option;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Option getOption() {
+		return option;
+	}
+
+	public void setOption(Option option) {
+		this.option = option;
+	}
+
 }
