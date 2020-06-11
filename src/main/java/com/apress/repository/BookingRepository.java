65bd6233a757e1327d7ef976ff95ebaf46921f56
SherlockFer <fermin.aguilar.b@gmail.com>
2020-06-11 09:44:34 +0100

    Setup MapStruct with Lombok

--- src/main/java/com/apress/repository/BookingRepository.java
+++ src/main/java/com/apress/repository/BookingRepository.java
@@ -1,11 +1,11 @@
 package com.apress.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.apress.domain.Booking;
 
 @Repository
-public interface BookingRepository extends CrudRepository<Booking, Long> {
+public interface BookingRepository extends JpaRepository<Booking, Long> {
 
 }
