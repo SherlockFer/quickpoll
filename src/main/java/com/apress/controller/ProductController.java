a2011812ef36f846a4a3e48fda883fab339facad
SherlockFer <fermin.aguilar.b@gmail.com>
2020-06-14 18:04:48 +0100

    Adding CRUD methods -> Products

--- src/main/java/com/apress/controller/ProductController.java
+++ src/main/java/com/apress/controller/ProductController.java
@@ -1,26 +1,82 @@
 package com.apress.controller;
 
+import java.net.URI;
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.apress.dto.ProductDTO;
+import com.apress.exception.ResourceNotFoundException;
 import com.apress.service.ProductService;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+
 @RestController
+@RequestMapping("/products")
 public class ProductController {
 	
 	@Autowired
 	private ProductService productService;
 
-	@GetMapping(value = "/products")
+	@GetMapping()
 	public ResponseEntity<Collection<ProductDTO>> getAllProducts() {
 		Collection<ProductDTO> productDTOs = productService.findAll();
 		return new ResponseEntity<>(productDTOs, HttpStatus.OK);
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+		verifyProduct(id);
+		Optional<ProductDTO> productDTO = productService.findById(id);
+		return new ResponseEntity<>(productDTO.get(), HttpStatus.OK);
+	}
+
+	@PostMapping()
+	public ResponseEntity<Void> create(@RequestBody ProductDTO productDTO) {
+		productDTO = productService.save(productDTO);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setLocation(buildLocationUri(productDTO.getId()));
+		return new ResponseEntity<>(headers, HttpStatus.CREATED);
+	}
+
+	private URI buildLocationUri(Long id) {
+		return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(id).toUri();
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody ProductDTO productDTO, @PathVariable Long id) {
+		verifyProduct(id);
+		productService.save(productDTO);
+		return new ResponseEntity<>(HttpStatus.OK);
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id) {
+		verifyProduct(id);
+		productService.deleteById(id);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
+
+	private void verifyProduct(Long id) {
+		Optional<ProductDTO> productDTO = productService.findById(id);
+		if (!productDTO.isPresent()) {
+			throw new ResourceNotFoundException(String.format("Product with id %s not found", id));
+		}
 	}
 
 }
--- src/main/java/com/apress/service/ProductService.java
+++ src/main/java/com/apress/service/ProductService.java
@@ -1,6 +1,7 @@
 package com.apress.service;
 
 import java.util.Collection;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,23 @@ public class ProductService {
 	public Collection<ProductDTO> findAll() {
 		Collection<Product> products = productRepository.findAll();
 		return productMapper.toProductDTOs(products);
+	}
+
+	public Optional<ProductDTO> findById(long id) {
+		Optional<Product> product = productRepository.findById(id);
+		if (product.isPresent()) {
+			return Optional.of(productMapper.toProductDTO(product.get()));
+		}
+		return Optional.empty();
+	}
+
+	public ProductDTO save(ProductDTO productDTO) {
+		Product product = productRepository.save(productMapper.toProduct(productDTO));
+		return productMapper.toProductDTO(product);
+	}
+
+	public void deleteById(Long productId) {
+		productRepository.deleteById(productId);
 	}
 
 }
