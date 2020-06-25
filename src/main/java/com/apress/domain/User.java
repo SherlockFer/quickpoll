4dee45278ae361fc1019c81e2e729b92fbfbfb64
SherlockFer <fermin.aguilar.b@gmail.com>
2020-06-25 09:46:19 -0500

    Revert "Agregando OneToMany a User - Bookings"
    This reverts commit b0190d7662f32d864831bf1753e470313acea491.

--- src/main/java/com/apress/domain/User.java
+++ src/main/java/com/apress/domain/User.java
@@ -1,15 +1,11 @@
 package com.apress.domain;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
@@ -22,7 +18,6 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.Singular;
 
 @Getter
 @Setter
@@ -45,16 +40,12 @@ public class User {
 	@Column(name = "EMAIL", nullable = false, updatable = false, unique = true)
 	private String email;
 
-	@Singular
-	@OneToMany
-	@JoinColumn(name = "booking_id")
-	private List<Booking> bookings = new ArrayList<>();
-
 	@Column(name = "FULL_NAME")
 	private String fullName;
 
 	@Column(name = "MOBILE")
 	private String mobile;
+
 
 	@Column(name = "ROLE")
 	private String role;
