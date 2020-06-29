9543a2effe296f9928e4ceb0d3597d066d7217e0
SherlockFer <fermin.aguilar.b@gmail.com>
2020-06-29 12:47:46 -0500

    Agregando validaciones a BookingValidator

--- src/main/java/com/apress/validation/BookingValidator.java
+++ src/main/java/com/apress/validation/BookingValidator.java
@@ -7,14 +7,28 @@ import com.apress.dto.BookingDTO;
 
 @Component
 public class BookingValidator {
-	
+
 	public void validate(BookingDTO bookingDTO) {
 		validateComments(bookingDTO);
+		validateVehiculeNumberPlate(bookingDTO);
+		validateStatus(bookingDTO);
 	}
 
 	private void validateComments(BookingDTO bookingDTO) {
-		if(StringUtils.isBlank(bookingDTO.getComments())) {
+		if (StringUtils.isBlank(bookingDTO.getComments())) {
 			bookingDTO.addError("Comments can't be empty");
+		}
+	}
+
+	private void validateVehiculeNumberPlate(BookingDTO bookingDTO) {
+		if (StringUtils.isBlank(bookingDTO.getVehiculeNumberPlate())) {
+			bookingDTO.addError("Vehicule number plate can't be empty");
+		}
+	}
+
+	private void validateStatus(BookingDTO bookingDTO) {
+		if (StringUtils.isBlank(bookingDTO.getStatus())) {
+			bookingDTO.addError("Status can't be empty");
 		}
 	}
 
