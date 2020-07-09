eabf4ff5b49f96c11e5a0c89c186de4bd7caf0be
SherlockFer <fermin.aguilar.b@gmail.com>
2020-07-08 23:52:34 -0500

    adding GeolocationCliente

---
+++ src/main/java/com/apress/client/GeoLocationClient.java
@@ -0,0 +1,38 @@
+package com.apress.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.stereotype.Service;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+import garage.services.geolocation.types.GetLocationRequest;
+import garage.services.geolocation.types.GetLocationResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+
+@Service
+public class GeoLocationClient {
+
+	private WebServiceTemplate webServiceTemplate;
+
+	@Autowired
+	public GeoLocationClient() {
+		this.webServiceTemplate = new WebServiceTemplate();
+		webServiceTemplate.setDefaultUri("http://localhost:8081/ws");
+		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+		marshaller.setPackagesToScan("garage.services.geolocation.types");
+		webServiceTemplate.setMarshaller(marshaller);
+		webServiceTemplate.setUnmarshaller(marshaller);
+	}
+
+	public GetLocationResponse getLocation(GetLocationRequest getLocationRequest) {
+		GetLocationResponse getLocationResponse = null;
+		try {
+			getLocationResponse = (GetLocationResponse) webServiceTemplate.marshalSendAndReceive(getLocationRequest);
+		} catch (RuntimeException exception) {
+			log.error("Vies Service unavailable", exception);
+		}
+		return getLocationResponse;
+	}
+}
