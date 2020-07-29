331cb4bf274d82d29c48c9a61786196b92a24be0
SherlockFer <fermin.aguilar.b@gmail.com>
2020-07-29 07:47:15 +0100

    Removing listener annotation

--- api/src/main/java/com/apress/producer/VehiclePlateProducer.java
+++ api/src/main/java/com/apress/producer/VehiclePlateProducer.java
@@ -1,6 +1,5 @@
 package com.apress.producer;
 
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,7 +13,6 @@ public class VehiclePlateProducer {
 	@Autowired
 	private RabbitTemplate rabbitTemplate;
 
-	@RabbitListener(queues = "myQueue")
 	public void sendVehiclePlate(String vehiclePlate) {
 		log.info("Sending Vehicle Plate details: {} " + vehiclePlate);
 		rabbitTemplate.convertAndSend("myQueue", vehiclePlate);
