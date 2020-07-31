aaa24f8468318e8cd7d416396c66a78db3d657b4
SherlockFer <fermin.aguilar.b@gmail.com>
2020-07-31 00:23:47 -0500

    fixing and RestTemplate POST

--- audit/src/main/java/com/garage/listener/PlateMessageListener.java
+++ audit/src/main/java/com/garage/listener/PlateMessageListener.java
@@ -1,7 +1,8 @@
-package com.garage.service;
+package com.garage.listener;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.garage.dto.AuditDTO;
 
@@ -15,6 +16,9 @@ public class PlateMessageListener {
 	@RabbitListener(queues = "garage.vehicle.plate")
 	public void receiveMessage(final AuditDTO auditDTO) {
 		log.info("Received message: {}", auditDTO);
+		RestTemplate restTemplate = new RestTemplate();
+		String a = restTemplate.postForObject("https://httpbin.org/post", auditDTO, String.class);
+		log.info("String: {}", a);
 	}
 
 }
