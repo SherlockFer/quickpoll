7fb9f124f1c82443315b1694ab5569bdd78696de
SherlockFer <fermin.aguilar.b@gmail.com>
2020-09-03 19:01:44 -0500

    adding class BookingTotal

---
+++ api/src/main/java/com/apress/calcTotal/BookingTotal.java
@@ -0,0 +1,36 @@
+package com.apress.calcTotal;
+
+import org.springframework.stereotype.Component;
+
+import com.apress.dto.BookingDTO;
+import com.apress.dto.PartDTO;
+import com.apress.dto.ProductDTO;
+
+@Component
+public class BookingTotal {
+
+	public void calcTotal(BookingDTO bookingDTO) {
+		double sum = 0;
+		if (bookingDTO.getBaseProduct() != null) {
+			if (bookingDTO.getBaseProduct().getPrice() != null) {
+				sum = sum + bookingDTO.getBaseProduct().getPrice();
+			}
+		}
+
+		if (bookingDTO.getExtraProducts() != null) {
+			for (ProductDTO product : bookingDTO.getExtraProducts()) {
+				sum = sum + product.getPrice();
+			}
+		}
+		if (bookingDTO.getParts() != null) {
+			for (PartDTO part : bookingDTO.getParts()) {
+				sum = sum + part.getPrice();
+			}
+
+		}
+
+		bookingDTO.setTotal(sum);
+
+	}
+
+}
