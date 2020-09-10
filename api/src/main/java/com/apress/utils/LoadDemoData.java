0e17e79e2bb1e8a5b5bf69f414d27ea154553f18
SherlockFer <fermin.aguilar.b@gmail.com>
2020-09-10 13:51:10 +0100

    LoadDemoData Editing to Enum variables

--- api/src/main/java/com/apress/utils/LoadDemoData.java
+++ api/src/main/java/com/apress/utils/LoadDemoData.java
@@ -8,6 +8,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
+import com.apress.constants.Constants.*;
 import com.apress.domain.Booking;
 import com.apress.domain.Part;
 import com.apress.domain.Product;
@@ -41,25 +42,26 @@ public class LoadDemoData implements ApplicationRunner {
 		log.info("Loading database");
 
 		User user1 = userRepository
-				.save(User.builder().role("admin").password("123456").fullName("Administrator").mobile("123456789").email("admin@garage.com").build());
+				.save(User.builder().role(UserRole.admin.name()).password("123456").fullName("Administrator").mobile("123456789")
+				.email("admin@garage.com").build());
 		User user2 = userRepository
-				.save(User.builder().password("123456").fullName("Mechanic-1").mobile("123456789").email("mechanic-1@garage.com")
-				.role("mechanic").build());
+				.save(User.builder().role(UserRole.mechanic.name()).password("123456").fullName("Mechanic-1").mobile("123456789")
+				.email("mechanic-1@garage.com").build());
 		User user3 = userRepository
-				.save(User.builder().password("123456").fullName("Mechanic-2").mobile("123456789").email("mechanic-2@garage.com")
-				.role("mechanic").build());
+				.save(User.builder().role(UserRole.mechanic.name()).password("123456").fullName("Mechanic-2").mobile("123456789")
+				.email("mechanic-2@garage.com").build());
 		User user4 = userRepository
-				.save(User.builder().password("123456").fullName("Customer-3").mobile("123456789").email("mechanic-3@garage.com")
-				.role("Customer").build());
+				.save(User.builder().role(UserRole.customer.name()).password("123456").fullName("Customer-3").mobile("123456789")
+				.email("mechanic-3@garage.com").build());
 		User user5 = userRepository
-				.save(User.builder().password("123456").fullName("Customer-1").mobile("123456789").email("Customer-1@garage.com")
-				.role("customer").build());
+				.save(User.builder().role(UserRole.customer.name()).password("123456").fullName("Customer-1").mobile("123456789")
+				.email("Customer-1@garage.com").build());
 		User user6 = userRepository
-				.save(User.builder().password("123456").fullName("Customer-2").mobile("123456789").email("Customer-2@garage.com")
-				.role("customer").build());
+				.save(User.builder().role(UserRole.customer.name()).password("123456").fullName("Customer-2").mobile("123456789")
+				.email("Customer-2@garage.com").build());
 		User user7 = userRepository
-				.save(User.builder().password("123456").fullName("Customer-3").mobile("123456789").email("Customer-3@garage.com")
-				.role("customer").build());
+				.save(User.builder().role(UserRole.customer.name()).password("123456").fullName("Customer-3").mobile("123456789")
+				.email("Customer-3@garage.com").build());
 
 		Product product1 = productRepository
 				.save(Product.builder().reference(UUID.randomUUID().toString()).name("Annual Service").category("base")
@@ -95,12 +97,12 @@ public class LoadDemoData implements ApplicationRunner {
 				.date(LocalDate.parse("2020-10-01"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 1")
-				.status("booked")
+				.status(BookingStatus.booked.name())
 				.vehicleBrand("Ford")
-				.vehicleEngine("diesel")
+				.vehicleEngine(VehicleEngine.diesel.name())
 				.vehicleModel("M5")
 				.vehicleNumberPlate("AAA-111")
-				.vehicleType("Car")
+				.vehicleType(VehicleType.car.name())
 				.part(part1)
 				.part(part2)
 				.mechanic(user2)
@@ -114,12 +116,12 @@ public class LoadDemoData implements ApplicationRunner {
 				.date(LocalDate.parse("2020-10-02"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 2")
-				.status("booked")
+				.status(BookingStatus.booked.name())
 				.vehicleBrand("Ford")
-				.vehicleEngine("diesel")
+				.vehicleEngine(VehicleEngine.diesel.name())
 				.vehicleModel("M5")
 				.vehicleNumberPlate("BBB-222")
-				.vehicleType("car")
+				.vehicleType(VehicleType.car.name())
 				.part(part1)
 				.mechanic(user2)
 				.customer(user1)
@@ -132,12 +134,12 @@ public class LoadDemoData implements ApplicationRunner {
 				.date(LocalDate.parse("2020-10-03"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 3")
-				.status("in_service")
+				.status(BookingStatus.in_service.name())
 				.vehicleBrand("Ford")
-				.vehicleEngine("petrol")
+				.vehicleEngine(VehicleEngine.petrol.name())
 				.vehicleModel("M5")
 				.vehicleNumberPlate("CCC-333")
-				.vehicleType("small_van")
+				.vehicleType(VehicleType.small_van.name())
 				.part(part1)
 				.mechanic(user2)
 				.customer(user1)
@@ -150,12 +152,12 @@ public class LoadDemoData implements ApplicationRunner {
 				.date(LocalDate.parse("2020-10-04"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 4")
-				.status("fixed")
+				.status(BookingStatus.fixed.name())
 				.vehicleBrand("Ford")
-				.vehicleEngine("petrol")
+				.vehicleEngine(VehicleEngine.hybrid.name())
 				.vehicleModel("M5")
 				.vehicleNumberPlate("DDD-444")
-				.vehicleType("small_bus")
+				.vehicleType(VehicleType.small_bus.name())
 				.part(part1)
 				.mechanic(user2)
 				.customer(user1)
@@ -168,12 +170,12 @@ public class LoadDemoData implements ApplicationRunner {
 				.date(LocalDate.parse("2020-10-05"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 5")
-				.status("collected")
+				.status(BookingStatus.collected.name())
 				.vehicleBrand("Ford")
-				.vehicleEngine("hybrid")
+				.vehicleEngine(VehicleEngine.electric.name())
 				.vehicleModel("M5")
 				.vehicleNumberPlate("EEE-666")
-				.vehicleType("small_bus")
+				.vehicleType(VehicleType.small_bus.name())
 				.part(part1)
 				.mechanic(user2)
 				.customer(user1)
@@ -186,12 +188,12 @@ public class LoadDemoData implements ApplicationRunner {
 				.date(LocalDate.parse("2020-10-06"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 6")
-				.status("unrepairable")
+				.status(BookingStatus.unrepairable.name())
 				.vehicleBrand("Ford")
-				.vehicleEngine("electric")
+				.vehicleEngine(VehicleEngine.electric.name())
 				.vehicleModel("M5")
 				.vehicleNumberPlate("FFF-777")
-				.vehicleType("car")
+				.vehicleType(VehicleType.car.name())
 				.part(part1)
 				.mechanic(user2)
 				.customer(user1)
