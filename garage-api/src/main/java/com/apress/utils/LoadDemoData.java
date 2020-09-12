1534a638da43f716037977972c0d9443e55c9fd5
SherlockFer <fermin.aguilar.b@gmail.com>
2020-09-12 16:42:36 +0100

    Formatting

--- garage-api/src/main/java/com/apress/utils/LoadDemoData.java
+++ garage-api/src/main/java/com/apress/utils/LoadDemoData.java
@@ -8,7 +8,10 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-import com.apress.constants.Constants.*;
+import com.apress.constants.Constants.BookingStatus;
+import com.apress.constants.Constants.UserRole;
+import com.apress.constants.Constants.VehicleEngine;
+import com.apress.constants.Constants.VehicleType;
 import com.apress.domain.Booking;
 import com.apress.domain.Part;
 import com.apress.domain.Product;
@@ -42,58 +45,147 @@ public class LoadDemoData implements ApplicationRunner {
 		log.info("Loading database");
 
 		User user1 = userRepository
-				.save(User.builder().role(UserRole.admin.name()).password("123456").fullName("Administrator").mobile("123456789")
-				.email("admin@garage.com").build());
+				.save(User.builder()
+				.role(UserRole.admin.name())
+				.password("123456")
+				.fullName("Administrator")
+				.mobile("123456789")
+				.email("admin@garage.com")
+				.build());
+		
 		User user2 = userRepository
-				.save(User.builder().role(UserRole.mechanic.name()).password("123456").fullName("Mechanic-1").mobile("123456789")
-				.email("mechanic-1@garage.com").build());
+				.save(User.builder()
+				.role(UserRole.mechanic.name())
+				.password("123456")
+				.fullName("Mechanic-1")
+				.mobile("123456789")
+				.email("mechanic-1@garage.com")
+				.build());
+		
 		User user3 = userRepository
-				.save(User.builder().role(UserRole.mechanic.name()).password("123456").fullName("Mechanic-2").mobile("123456789")
-				.email("mechanic-2@garage.com").build());
+				.save(User.builder()
+				.role(UserRole.mechanic.name())
+				.password("123456")
+				.fullName("Mechanic-2")
+				.mobile("123456789")
+				.email("mechanic-2@garage.com")
+				.build());
+		
 		User user4 = userRepository
-				.save(User.builder().role(UserRole.customer.name()).password("123456").fullName("Customer-3").mobile("123456789")
-				.email("mechanic-3@garage.com").build());
+				.save(User.builder()
+				.role(UserRole.customer.name())
+				.password("123456")
+				.fullName("Customer-3")
+				.mobile("123456789")
+				.email("mechanic-3@garage.com")
+				.build());
+		
 		User user5 = userRepository
-				.save(User.builder().role(UserRole.customer.name()).password("123456").fullName("Customer-1").mobile("123456789")
-				.email("Customer-1@garage.com").build());
+				.save(User.builder()
+				.role(UserRole.customer.name())
+				.password("123456")
+				.fullName("Customer-1")
+				.mobile("123456789")
+				.email("customer-1@garage.com")
+				.build());
+		
 		User user6 = userRepository
-				.save(User.builder().role(UserRole.customer.name()).password("123456").fullName("Customer-2").mobile("123456789")
-				.email("Customer-2@garage.com").build());
+				.save(User.builder()
+				.role(UserRole.customer.name())
+				.password("123456")
+				.fullName("Customer-2")
+				.mobile("123456789")
+				.email("customer-2@garage.com")
+				.build());
+		
 		User user7 = userRepository
-				.save(User.builder().role(UserRole.customer.name()).password("123456").fullName("Customer-3").mobile("123456789")
-				.email("Customer-3@garage.com").build());
+				.save(User.builder()
+				.role(UserRole.customer.name())
+				.password("123456")
+				.fullName("Customer-3")
+				.mobile("123456789")
+				.email("customer-3@garage.com")
+				.build());
 
 		Product product1 = productRepository
-				.save(Product.builder().reference(UUID.randomUUID().toString()).name("Annual Service").category("base")
-						.price(50).build());
+				.save(Product.builder()
+				.reference(UUID.randomUUID().toString())
+				.name("Annual Service")
+				.category("base")
+				.price(50)
+				.build());
+		
 		Product product2 = productRepository
-				.save(Product.builder().reference(UUID.randomUUID().toString()).name("Major Service").category("base")
-						.price(60).build());
+				.save(Product.builder()
+				.reference(UUID.randomUUID().toString())
+				.name("Major Service")
+				.category("base")
+				.price(60)
+				.build());
+		
 		Product product3 = productRepository
-				.save(Product.builder().reference(UUID.randomUUID().toString()).name("Repair or Fault").category("base")
-						.price(70).build());
+				.save(Product.builder()
+				.reference(UUID.randomUUID().toString())
+				.name("Repair or Fault")
+				.category("base")
+				.price(70)
+				.build());
+		
 		Product product4 = productRepository
-				.save(Product.builder().reference(UUID.randomUUID().toString()).name("Major Repair").category("base")
-						.price(80).build());
+				.save(Product.builder()
+				.reference(UUID.randomUUID().toString())
+				.name("Major Repair")
+				.category("base")
+				.price(80)
+				.build());
+		
 		Product product5 = productRepository
-				.save(Product.builder().reference(UUID.randomUUID().toString()).name("Wheel alignment")
-						.category("extra").price(90).build());
+				.save(Product.builder()
+				.reference(UUID.randomUUID().toString())
+				.name("Wheel alignment")
+				.category("extra")
+				.price(90)
+				.build());
+		
 		Product product6 = productRepository
-				.save(Product.builder().reference(UUID.randomUUID().toString()).name("Grease and lubricat")
-						.category("extra").price(70).build());
+				.save(Product.builder()
+				.reference(UUID.randomUUID().toString())
+				.name("Grease and lubricat")
+				.category("extra")
+				.price(70)
+				.build());
+		
 		Product product7 = productRepository
-				.save(Product.builder().reference(UUID.randomUUID().toString()).name("Suspension").category("extra")
-						.price(70).build());
+				.save(Product.builder()
+				.reference(UUID.randomUUID().toString())
+				.name("Suspension")
+				.category("extra")
+				.price(70)
+				.build());
 
 		Part part1 = partRepository
-				.save(Part.builder().sku(UUID.randomUUID().toString()).name("Engine motor oil").price(50).build());
+				.save(Part.builder()
+				.sku(UUID.randomUUID().toString())
+				.name("Engine motor oil")
+				.price(50)
+				.build());
+		
 		Part part2 = partRepository
-				.save(Part.builder().sku(UUID.randomUUID().toString()).name("Filter oil").price(60).build());
+				.save(Part.builder()
+				.sku(UUID.randomUUID().toString())
+				.name("Filter oil")
+				.price(60)
+				.build());
+		
 		Part part3 = partRepository
-				.save(Part.builder().sku(UUID.randomUUID().toString()).name("Filer ai").price(70).build());
+				.save(Part.builder()
+				.sku(UUID.randomUUID().toString())
+				.name("Filer ai")
+				.price(70)
+				.build());
 
-		Booking booking1 = bookingRepository.save(
-				Booking.builder()
+		Booking booking1 = bookingRepository
+				.save(Booking.builder()
 				.date(LocalDate.parse("2020-10-01"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 1")
@@ -109,10 +201,11 @@ public class LoadDemoData implements ApplicationRunner {
 				.customer(user1)
 				.baseProduct(product1)
 				.extraProduct(product2)
-				.extraProduct(product3).build());
+				.extraProduct(product3)
+				.build());
 		
-		Booking booking2 = bookingRepository.save(
-				Booking.builder()
+		Booking booking2 = bookingRepository
+				.save(Booking.builder()
 				.date(LocalDate.parse("2020-10-02"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 2")
@@ -127,10 +220,11 @@ public class LoadDemoData implements ApplicationRunner {
 				.customer(user1)
 				.baseProduct(product1)
 				.extraProduct(product2)
-				.extraProduct(product3).build());
+				.extraProduct(product3)
+				.build());
 		
-		Booking booking3 = bookingRepository.save(
-				Booking.builder()
+		Booking booking3 = bookingRepository
+				.save(Booking.builder()
 				.date(LocalDate.parse("2020-10-03"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 3")
@@ -145,10 +239,11 @@ public class LoadDemoData implements ApplicationRunner {
 				.customer(user1)
 				.baseProduct(product1)
 				.extraProduct(product2)
-				.extraProduct(product3).build());
+				.extraProduct(product3)
+				.build());
 		
-		Booking booking4 = bookingRepository.save(
-				Booking.builder()
+		Booking booking4 = bookingRepository
+				.save(Booking.builder()
 				.date(LocalDate.parse("2020-10-04"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 4")
@@ -163,10 +258,11 @@ public class LoadDemoData implements ApplicationRunner {
 				.customer(user1)
 				.baseProduct(product1)
 				.extraProduct(product2)
-				.extraProduct(product3).build());
+				.extraProduct(product3)
+				.build());
 		
-		Booking booking5 = bookingRepository.save(
-				Booking.builder()
+		Booking booking5 = bookingRepository
+				.save(Booking.builder()
 				.date(LocalDate.parse("2020-10-05"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 5")
@@ -181,10 +277,11 @@ public class LoadDemoData implements ApplicationRunner {
 				.customer(user1)
 				.baseProduct(product1)
 				.extraProduct(product2)
-				.extraProduct(product3).build());
+				.extraProduct(product3)
+				.build());
 		
-		Booking booking6 = bookingRepository.save(
-				Booking.builder()
+		Booking booking6 = bookingRepository
+				.save(Booking.builder()
 				.date(LocalDate.parse("2020-10-06"))
 				.reference(UUID.randomUUID().toString())
 				.comments("comentario 6")
@@ -199,7 +296,8 @@ public class LoadDemoData implements ApplicationRunner {
 				.customer(user1)
 				.baseProduct(product1)
 				.extraProduct(product2)
-				.extraProduct(product3).build());
+				.extraProduct(product3)
+				.build());
 		
 	}
 }
