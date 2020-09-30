4b6c244d3f5df44469fab88e7263526ec851ce7f
Braiyan <brian.aguilar.b@gmail.com>
2020-09-30 00:08:42 -0500

    adding BookingNotificationScheduler

---
+++ garage-api/src/main/java/com/apress/schedule/BookingNotificationScheduler.java
@@ -0,0 +1,24 @@
+package com.apress.schedule;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.apress.repository.BookingRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+
+@Service
+public class BookingNotificationScheduler {
+
+	@Autowired
+	private BookingRepository bookingRepository;
+
+	@Scheduled(cron = "* 3 * * * *", zone = "Europe/Dublin")
+	public void sendPendingBookings() {
+		log.info("Pending Bookings: {} " + bookingRepository.count());
+	}
+
+}
